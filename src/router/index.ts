8ec30dc883b8616b55f6355989d557594a676693
Stefan Wiedemann <wistefan@googlemail.com>
2026-04-22 07:50:53 +0200

    change import

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
 import {
   createRouter,
   createWebHistory,
